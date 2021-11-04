e5f3c7d48de21b73519c790c7896a6ed104ee6b8
George-T800 <giorgi.takadze2013@ens.tsu.edu.ge>
2021-11-04 13:43:07 +0400

    hello word

---
+++ ob/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Made By Cipher"
+echo "Crypton"
+echo "Object detection " 
+
+
+python3 real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel 
+
